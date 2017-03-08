fffea05e2655ea9bd6555f22461277916a4ecf4e
erramuzpe <asier.erramuzpe@gmail.com>
2017-03-08 11:56:42 +0100

    deleted not neccesary files

--- time/creates_cube2D.m
+++ time/creates_cube2D.m
@@ -6,36 +6,20 @@
 % Necessary function to run simula2D.m
 
 
-function [cube,list] = creates_random_ITH_cube2D(L,H,C)
-
+function [cube] = creates_cube2D(L,H,C)
 
 cube=zeros(L,L);
 cont=0;
-list=[];
-
-
 
 while (cont<H)
-    
     x=floor(rand(1,1).*(L))+1;
     y=floor(rand(1,1).*(L))+1;
     c=floor(rand(1,1).*(C))+1;
 
-    
-    
     if cube(x,y) ~= 0
-    else 
-            cube(x,y)=c;
-            cont=cont+1;
-            
-            list=[list; x y];
-            
-        
+    else
+        cube(x,y)=c;
+        cont=cont+1;    
     end
-    
-    
-    
 end
-
-
 end
