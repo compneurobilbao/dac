9c7d659bd9d078c9bc38d01ff0de37aeb91298ee
erramuzpe <asier.erramuzpe@gmail.com>
2017-03-14 10:50:03 +0100

    correct figures

--- time/figures/figure_2.m
+++ time/figures/figure_2.m
@@ -4,68 +4,95 @@ clc;
 load('linear.mat')
 
 for c=1:4
-    data_rp(:,c) = data(:,4*(c-1)+2);
-    data_dac(:,c) = data(:,4*(c-1)+4);
+    rp_mean(:,c) = data(:,4*(c-1)+2);
+    dac_mean(:,c) = data(:,4*(c-1)+4);
+    rp_std(:,c) = sqrt(data(:,4*(c-1)+3));
+    dac_std(:,c) = sqrt(data(:,4*(c-1)+5));
 end
-rp_mean = mean(data_rp);
-dac_mean = mean(data_dac);
-rp_std = std(data_rp);
-dac_std = std(data_dac);
 
-
-hb = bar([rp_mean' dac_mean']);
+subplot(1,2,1)
+hb = bar([rp_mean(10,:)' dac_mean(10,:)']);
 set(hb(1),'FaceColor','b');
 set(hb(2),'FaceColor','r');
 legend('rp','dac');
-set(gca, 'FontSize',12,'XTick',[1 2 3 4],'XTickLabel',{'ITH 1','ITH 2','ITH 3','ITH 4' });
+set(gca, 'FontSize',12,'XTick',[1 2 3 4],'XTickLabel', ...
+    {'ITH 1','ITH 2','ITH 3','ITH 4'});
 ylabel('Mean performance')
 
-errbar = [rp_std; dac_std];   % CREATE ‘errbar’ MATRIX
-yd = [rp_mean' dac_mean' ]';
+errbar = [rp_std(10,:); dac_std(10,:)];   % CREATE ‘errbar’ MATRIX
+yd = [rp_mean(10,:); dac_mean(10,:)];
 hold on
 for k1 = 1:2
     errorbar([1:4]+.15*(k1-2),  yd(k1,:),  errbar(k1,:), '.k', 'LineWidth',2)
 end
+
+subplot(1,2,2)
+hb = bar([rp_mean(90,:)' dac_mean(90,:)']);
+set(hb(1),'FaceColor','b');
+set(hb(2),'FaceColor','r');
+legend('rp','dac');
+set(gca, 'FontSize',12,'XTick',[1 2 3 4],'XTickLabel', ...
+    {'ITH 1','ITH 2','ITH 3','ITH 4'});
+ylabel('Mean performance')
+
+errbar = [rp_std(90,:); dac_std(90,:)];   % CREATE ‘errbar’ MATRIX
+yd = [rp_mean(90,:); dac_mean(90,:)];
+hold on
+for k1 = 1:2
+    errorbar([1:4]+.15*(k1-2),  yd(k1,:),  errbar(k1,:), '.k', 'LineWidth',2)
+end
+ylim([0 120]);
 hold off
 print('linear_fig2','-depsc')
 close all
 
 
-
-
 clear all;
 clc;
-
 load('branch.mat')
 
 for c=1:4
-    data_rp(:,c) = data(:,4*(c-1)+2);
-    data_dac(:,c) = data(:,4*(c-1)+4);
+    rp_mean(:,c) = data(:,4*(c-1)+2);
+    dac_mean(:,c) = data(:,4*(c-1)+4);
+    rp_std(:,c) = sqrt(data(:,4*(c-1)+3));
+    dac_std(:,c) = sqrt(data(:,4*(c-1)+5));
 end
-rp_mean = mean(data_rp);
-dac_mean = mean(data_dac);
-rp_std = std(data_rp);
-dac_std = std(data_dac);
 
-
-hb = bar([rp_mean' dac_mean']);
+subplot(1,2,1)
+hb = bar([rp_mean(10,:)' dac_mean(10,:)']);
 set(hb(1),'FaceColor','b');
 set(hb(2),'FaceColor','r');
 legend('rp','dac');
-set(gca, 'FontSize',12,'XTick',[1 2 3 4],'XTickLabel',{'ITH 1','ITH 2','ITH 3','ITH 4' });
+set(gca, 'FontSize',12,'XTick',[1 2 3 4],'XTickLabel', ...
+    {'ITH 1','ITH 2','ITH 3','ITH 4'});
 ylabel('Mean performance')
 
-errbar = [rp_std; dac_std];   % CREATE ‘errbar’ MATRIX
-yd = [rp_mean' dac_mean' ]';
+errbar = [rp_std(10,:); dac_std(10,:)];   % CREATE ‘errbar’ MATRIX
+yd = [rp_mean(10,:); dac_mean(10,:)];
 hold on
 for k1 = 1:2
     errorbar([1:4]+.15*(k1-2),  yd(k1,:),  errbar(k1,:), '.k', 'LineWidth',2)
 end
+
+subplot(1,2,2)
+hb = bar([rp_mean(90,:)' dac_mean(90,:)']);
+set(hb(1),'FaceColor','b');
+set(hb(2),'FaceColor','r');
+legend('rp','dac');
+set(gca, 'FontSize',12,'XTick',[1 2 3 4],'XTickLabel', ...
+    {'ITH 1','ITH 2','ITH 3','ITH 4'});
+ylabel('Mean performance')
+
+errbar = [rp_std(90,:); dac_std(90,:)];   % CREATE ‘errbar’ MATRIX
+yd = [rp_mean(90,:); dac_mean(90,:)];
+hold on
+for k1 = 1:2
+    errorbar([1:4]+.15*(k1-2),  yd(k1,:),  errbar(k1,:), '.k', 'LineWidth',2)
+end
+ylim([-20 120]);
 hold off
 print('branch_fig2','-depsc')
 close all
-
-
 
 
 
@@ -75,28 +102,44 @@ clc;
 load('neutral.mat')
 
 for c=1:4
-    data_rp(:,c) = data(:,4*(c-1)+2);
-    data_dac(:,c) = data(:,4*(c-1)+4);
+    rp_mean(:,c) = data(:,4*(c-1)+2);
+    dac_mean(:,c) = data(:,4*(c-1)+4);
+    rp_std(:,c) = sqrt(data(:,4*(c-1)+3));
+    dac_std(:,c) = sqrt(data(:,4*(c-1)+5));
 end
-rp_mean = mean(data_rp);
-dac_mean = mean(data_dac);
-rp_std = std(data_rp);
-dac_std = std(data_dac);
 
-
-hb = bar([rp_mean' dac_mean']);
+subplot(1,2,1)
+hb = bar([rp_mean(10,:)' dac_mean(10,:)']);
 set(hb(1),'FaceColor','b');
 set(hb(2),'FaceColor','r');
 legend('rp','dac');
-set(gca, 'FontSize',12,'XTick',[1 2 3 4],'XTickLabel',{'ITH 1','ITH 2','ITH 3','ITH 4' });
+set(gca, 'FontSize',12,'XTick',[1 2 3 4],'XTickLabel', ...
+    {'ITH 1','ITH 2','ITH 3','ITH 4'});
 ylabel('Mean performance')
 
-errbar = [rp_std; dac_std];   % CREATE ‘errbar’ MATRIX
-yd = [rp_mean' dac_mean' ]';
+errbar = [rp_std(10,:); dac_std(10,:)];   % CREATE ‘errbar’ MATRIX
+yd = [rp_mean(10,:); dac_mean(10,:)];
 hold on
 for k1 = 1:2
     errorbar([1:4]+.15*(k1-2),  yd(k1,:),  errbar(k1,:), '.k', 'LineWidth',2)
 end
+
+subplot(1,2,2)
+hb = bar([rp_mean(90,:)' dac_mean(90,:)']);
+set(hb(1),'FaceColor','b');
+set(hb(2),'FaceColor','r');
+legend('rp','dac');
+set(gca, 'FontSize',12,'XTick',[1 2 3 4],'XTickLabel', ...
+    {'ITH 1','ITH 2','ITH 3','ITH 4'});
+ylabel('Mean performance')
+
+errbar = [rp_std(90,:); dac_std(90,:)];   % CREATE ‘errbar’ MATRIX
+yd = [rp_mean(90,:); dac_mean(90,:)];
+hold on
+for k1 = 1:2
+    errorbar([1:4]+.15*(k1-2),  yd(k1,:),  errbar(k1,:), '.k', 'LineWidth',2)
+end
+ylim([0 120]);
 hold off
 print('neutral_fig2','-depsc')
 close all
@@ -107,28 +150,47 @@ clc;
 load('punctuated.mat')
 
 for c=1:4
-    data_rp(:,c) = data(:,4*(c-1)+2);
-    data_dac(:,c) = data(:,4*(c-1)+4);
+    rp_mean(:,c) = data(:,4*(c-1)+2);
+    dac_mean(:,c) = data(:,4*(c-1)+4);
+    rp_std(:,c) = sqrt(data(:,4*(c-1)+3));
+    dac_std(:,c) = sqrt(data(:,4*(c-1)+5));
 end
-rp_mean = mean(data_rp);
-dac_mean = mean(data_dac);
-rp_std = std(data_rp);
-dac_std = std(data_dac);
 
 
-hb = bar([rp_mean' dac_mean']);
+subplot(1,2,1)
+hb = bar([rp_mean(10,:)' dac_mean(10,:)']);
 set(hb(1),'FaceColor','b');
 set(hb(2),'FaceColor','r');
 legend('rp','dac');
-set(gca, 'FontSize',12,'XTick',[1 2 3 4],'XTickLabel',{'ITH 1','ITH 2','ITH 3','ITH 4' });
+set(gca, 'FontSize',12,'XTick',[1 2 3 4],'XTickLabel', ...
+    {'ITH 1','ITH 2','ITH 3','ITH 4'});
 ylabel('Mean performance')
 
-errbar = [rp_std; dac_std];   % CREATE ‘errbar’ MATRIX
-yd = [rp_mean' dac_mean' ]';
+errbar = [rp_std(10,:); dac_std(10,:)];   % CREATE ‘errbar’ MATRIX
+yd = [rp_mean(10,:); dac_mean(10,:)];
 hold on
 for k1 = 1:2
     errorbar([1:4]+.15*(k1-2),  yd(k1,:),  errbar(k1,:), '.k', 'LineWidth',2)
 end
+ylim([0 120]);
+
+
+subplot(1,2,2)
+hb = bar([rp_mean(90,:)' dac_mean(90,:)']);
+set(hb(1),'FaceColor','b');
+set(hb(2),'FaceColor','r');
+legend('rp','dac');
+set(gca, 'FontSize',12,'XTick',[1 2 3 4],'XTickLabel', ...
+    {'ITH 1','ITH 2','ITH 3','ITH 4'});
+ylabel('Mean performance')
+
+errbar = [rp_std(90,:); dac_std(90,:)];   % CREATE ‘errbar’ MATRIX
+yd = [rp_mean(90,:); dac_mean(90,:)];
+hold on
+for k1 = 1:2
+    errorbar([1:4]+.15*(k1-2),  yd(k1,:),  errbar(k1,:), '.k', 'LineWidth',2)
+end
+ylim([0 120]);
 hold off
 print('punctuated_fig2','-depsc')
 close all
