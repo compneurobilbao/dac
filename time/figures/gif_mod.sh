091f66a66be0294ccf8caae13b594d7c6dd2a8d5
erramuzpe <asier.erramuzpe@gmail.com>
2017-03-20 12:19:14 +0100

    script modified

--- time/figures/gif_mod.sh
+++ time/figures/gif_mod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo gifsicle linear_performance.gif --delete "#0" --resize 700x420 -o linear_performance.gif
-sudo gifsicle branch_performance.gif --delete "#0" --resize 700x420 -o branch_performance.gif
-sudo gifsicle neutral_performance.gif --delete "#0" --resize 700x420 -o neutral_performance.gif 
-sudo gifsicle punctuated_performance.gif --delete "#0" --resize 700x420 -o punctuated_performance.gif
+sudo gifsicle linear_performance.gif --delete "#0"  -o linear_performance.gif
+sudo gifsicle branch_performance.gif --delete "#0"  -o branch_performance.gif
+sudo gifsicle neutral_performance.gif --delete "#0"  -o neutral_performance.gif 
+sudo gifsicle punctuated_performance.gif --delete "#0" -o punctuated_performance.gif
